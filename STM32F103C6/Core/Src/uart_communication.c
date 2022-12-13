a8f3a9cf12776ee4ec50bb9d8d1ad7bd858a6553
Nguyen Hoang Minh Tri <tri.nguyenhoangminh@hcmut.edu.vn>
2022-12-13 07:30:36 +0700

    First commit

---
+++ STM32F103C6/Core/Src/uart_communication.c
@@ -0,0 +1,51 @@
+/*
+ * uart_communication.c
+ *
+ *  Created on: Dec 13, 2022
+ *      Author: Hoang Tri
+ */
+
+
+#include "uart_communication.h"
+#include "string.h"
+#include "main.h"
+#include "global.h"
+#include "stdio.h"
+
+void uart_communication_fsm(){
+	char str[50];
+	switch (communication_status) {
+		case RECEIVE_COMMAND:
+			HAL_ADC_Start(&hadc1);
+			ADC_Value= HAL_ADC_GetValue(&hadc1);
+			HAL_ADC_Stop(&hadc1);
+			if (command_flag==1) {
+				uint8_t temp[MAX_BUFFER_SIZE]="!RST#";
+				if (compare(command_data, temp)==1) {
+					communication_status=SEND_ADC;
+					setTimer1(1);
+				}
+				command_flag=0;
+			}
+
+			break;
+		case SEND_ADC:
+			if (command_flag==1) {
+				uint8_t temp[MAX_BUFFER_SIZE]="!OK#";
+				if (compare(command_data, temp)==1) {
+					setTimer1(0);
+					communication_status=RECEIVE_COMMAND;
+				}
+				command_flag=0;
+			}
+			if (timer1_flag==1) {
+
+				HAL_UART_Transmit (&huart2 , (void*)str , sprintf (str , "\n%ld\n", ADC_Value), 1000);
+				setTimer1(300);
+			}
+
+			break;
+		default:
+			break;
+	}
+}
